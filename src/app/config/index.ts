2b38f1354049a186e4db8b506e9e565dd6cf0383
MuhammadJunaid01 <m.junaidbkh2020@gmail.com>
2025-01-18 21:14:30 +0600

    Refactor database URL configuration to use environment-specific variables for production and local environments

--- src/app/config/index.ts
+++ src/app/config/index.ts
@@ -1,10 +1,14 @@
 import dotenv from "dotenv";
 import path from "path";
 dotenv.config({ path: path.join(process.cwd(), ".env") });
+const databaseUrl =
+  process.env.NODE_ENV === "production"
+    ? process.env.DATABASE_URL_PROD
+    : process.env.DATABASE_URL_LOCAL;
 export default {
   NODE_ENV: process.env.NODE_ENV,
   port: process.env.PORT,
-  database_url: process.env.DATABASE_URL_LOCAL,
+  database_url: databaseUrl,
   secret: process.env.SECRET_TOKEN,
   secret_refresh: process.env.SECRET_REFRESH,
   email_user: process.env.EMAIL_USER,
